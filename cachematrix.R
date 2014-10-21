d280b2b9ed9c259522486f8c790ebb6082ca605a
mausumikumari <mausumi.kumari@gmail.com>
2014-10-21 14:56:30 +0530

    Revert "Revert "inverseMAtrix""
    This reverts commit b2f1255a25bd6a34fa714cfd98cc3f42b218c24e.

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,32 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+
+	inverse<-NULL
+
+	set<- function(matrix){
+
+	x<<-matrix
+	inverse<<-NULL
+
+	}
+
+	get<-function(){
+		x
+	}
+
+	setInverse<-function(i){
+		inverse<<-i
+	}
+	
+	getInverse<-function(){
+		inverse
+	}
+
+	list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
+
+
+
 }
 
 
@@ -12,4 +38,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+
+		inv<-x$getInverse()
+
+		if(!is.null(inv))
+		{
+			message("Getting cached data!")
+			return(inv)
+		}
+
+
+		matrix<-x$get()
+
+		inv<-solve(matrix)
+
+		x$setInverse(inv)
+
+		inv
 }
