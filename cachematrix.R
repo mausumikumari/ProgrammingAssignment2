6f6118b06e1382612c908cd9594b235fc3530f31
jinkapartha <jinka.partha@gmail.com>
2014-10-23 15:09:51 +0530

    inverseMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Does caching of inverse of the matrix. If inverse is not found,calculates the inverse and caches the inverse .
 
-## Write a short comment describing this function
-
+##This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
 
 
 	inverse<-NULL
 
+##seta the matrix object
 	set<- function(matrix){
 
 	x<<-matrix
@@ -15,14 +14,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 	}
 
+##to get the matrix
 	get<-function(){
 		x
 	}
 
+
+## set the inverse for the matrix
 	setInverse<-function(i){
 		inverse<<-i
 	}
-	
+
+## get the inverse for the matrix	
 	getInverse<-function(){
 		inverse
 	}
@@ -34,7 +37,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -42,16 +45,21 @@ cacheSolve <- function(x, ...) {
 
 		inv<-x$getInverse()
 
+
+	## checks whethwe inverse is present in the list
 		if(!is.null(inv))
 		{
 			message("Getting cached data!")
 			return(inv)
 		}
 
+	## if no inverse , calculate inverse using solve() function
 
 		matrix<-x$get()
 
 		inv<-solve(matrix)
+
+	## inverse is set in the list	using setInverse function.	
 
 		x$setInverse(inv)
 
